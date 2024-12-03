ec506f9f0bc73f408c8550aaebce5fd880aab020
samya-roy <samyaroy999@gmail.com>
2024-12-03 23:37:53 +0600

    route file added

---
+++ app/(routes)/allTask/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>all task</div>;
+};
+
+export default page;
---
+++ app/(routes)/important/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>importants</div>;
+};
+
+export default page;
